0ddce17fdf7a769cafa2f7dc246412d4aa794250
iamjoker021 <iamjoker021@gmail.com>
2024-08-06 16:15:27 +0530

    feat: Add SwaggerUI to document the API

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const busStopRouter = require('./router/busStopRouter');
+const swagger = require('./config/swagger');
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => res.json('App is running'));
+swagger(app);
+
 app.use('/api/bus-route', busStopRouter);
 
 const PORT = process.env.PORT || 3000;
---
+++ config/swagger.js
@@ -0,0 +1,24 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+      info: {
+        title: "Conductor App",
+        version: "0.0.0",
+        description: "This is backend api for Conducotr App",
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./router/*.js"],
+};
+
+const specs = swaggerJsdoc(options)
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+}
