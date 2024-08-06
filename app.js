dad3eac2d3c2311494033acdae38d1c66ee493e3
iamjoker021 <iamjoker021@gmail.com>
2024-08-06 11:43:31 +0530

    init: Install dependecies and create basic app

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json('App is running'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
