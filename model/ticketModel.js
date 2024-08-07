9739ffa1ee8ed46ea64318012b5661b87746156d
iamjoker021 <iamjoker021@gmail.com>
2024-08-07 14:51:37 +0530

    fix: Fix the no response on getTicketDetails and remove isValid portion of query since we are removing that column from DB
    The issue is await is missed on the query

--- model/ticketModel.js
+++ model/ticketModel.js
@@ -7,33 +7,31 @@ const getTicketDetailsForUser = async (userId, isValid) => {
         getTicketQ = `
         SELECT * FROM user_ticket.tickets 
         WHERE user_id = $1 
-        AND is_valid = true 
-        AND expiry_time > current_time()
-        ORDER BY issue_time desc
+        AND expiry_time > current_time
+        ORDER BY issue_time DESC;
         `;
     }
     else {
-        // Get alld tickets
+        // Get all tickets
         getTicketQ =  `
         SELECT * FROM user_ticket.tickets 
-        WHERE user_id = $1 
-        ORDER BY issue_time DESC
+        WHERE user_id = $1
+        ORDER BY issue_time ASC;
         `
     }
-    const { rows } = pool.query(getTicketQ, [userId]);
+    const { rows } = await pool.query(getTicketQ, [userId]);
     return rows;
 }
 
 const createTicketForUser = async (userId, tripDetails) => {
     const createTicketForUserQ = `
-    INSERT INTO user_ticket.tickets (ticket_qr, issue_time, expiry_time, trip_details, is_valid, user_id)
-    VALUES ($1, $2, $3, $4, $5, $6)
+    INSERT INTO user_ticket.tickets (ticket_qr, issue_time, expiry_time, trip_details, user_id)
+    VALUES ($1, $2, $3, $4, $5)
     `
     const ticket_qr = 'fill QR here';
     const currentTime = new Date();
     const expiryTime = new Date(currentTime.getTime() + ( 1 * 60 * 60 * 1000));
-    const isValid = true;
-    await pool.query(createTicketForUserQ, [ticket_qr, currentTime, expiryTime, tripDetails, isValid, userId]);
+    await pool.query(createTicketForUserQ, [ticket_qr, currentTime, expiryTime, tripDetails, userId]);
 }
 
 module.exports = {
